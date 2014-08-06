4e6fc825b3cc53b50fd405be54c60f3e5855e956
Jiang Lee <187378726@qq.com>
2014-08-06 17:34:42 +0800

    delete some files

--- www/mysql_test.php
+++
@@ -1,8 +0,0 @@
-<?php
-$con = mysql_connect("localhost","root","");
-if (!$con){
-  die('Could not connect: ' . mysql_error());
-} else {
-  echo 'Database connected successfully';
-}
-mysql_close($con);
